cf62065fe1096c16fdda26ee370fce1fd7a5e196
M-Hosny <muhammedahosny98@gmail.com>
2021-12-23 20:59:25 +0200

    Modification: This is the time and length scenes in the calculator's unit conversion mode

---
+++ Converter_FXMLController_Time_And_Length.java
@@ -0,0 +1,26 @@
+package calc_gui;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleButton;
+
+public class Converter_FXMLController_Time implements Initializable {
+
+    @FXML
+    private ToggleButton theme;
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+
+    @FXML
+    private void theme_change(ActionEvent event) {
+    }
+    
+}
