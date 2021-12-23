a2850dc23a1ee6881e3c1f9af3037de09edecfe7
M-Hosny <muhammedahosny98@gmail.com>
2021-12-23 20:14:18 +0200

    This is the time scene in the calculator's unit conversion mode

---
+++ Calc_GUI_Time.java
@@ -0,0 +1,23 @@
+
+package calc_gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Calc_GUI_Time extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Converter_FXML_Time.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+}
