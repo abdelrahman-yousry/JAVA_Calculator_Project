ba149250fc832969b12313e324e627ca315ea496
Yomna-AlShaboury <yomna.alshaboury@gmail.com>
2022-01-11 09:29:02 +0200

    edited they keys, edited the pins to match our Arduino Mega 2560

--- Two_keypad_arduino/Two_keypad_arduino.ino
+++ Two_keypad_arduino/Two_keypad_arduino.ino
@@ -3,36 +3,34 @@
 const int ROW_NUM = 4; //four rows
 const int COLUMN_NUM = 4; //four columns
 
-char keys1[ROW_NUM][COLUMN_NUM] = {
+char keys_1[ROW_NUM][COLUMN_NUM] = {
   {'<','u','>', 'A'},
   {'l','o','r', 'B'},
-  {'%','k','i', 'C'},
+  {'t','d','a', 'C'},
   {'c','F','E', 'D'}
 };
-char keys2[ROW_NUM][COLUMN_NUM]= {
-  {'1','2','3', '+'},
+char keys_2[ROW_NUM][COLUMN_NUM]= {
+  {'1','2','3', '/'},
   {'4','5','6', '*'},
-  {'7','8','9', '/'},
-  {'.','0','=', '-'}
+  {'7','8','9', '-'},
+  {'.','0','=', '+'}
 };
-byte pin_rows[ROW_NUM] = {4, 3, 2, 1}; //connect to the row pinouts of the keypad
-byte pin_column[COLUMN_NUM] = {8, 7, 6, 5}; //connect to the column pinouts of the keypad
-byte pin_rows[ROW_NUM] = {12, 11, 10, 9}; //connect to the row pinouts of the keypad
-byte pin_column1[COLUMN_NUM] = {16, 15, 14, 13}; //connect to the column pinouts of the keypad
-Keypad keypad1 = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
-Keypad keypad2= Keypad( makeKeymap(keys1), pin_rows, pin_column1, ROW_NUM, COLUMN_NUM );
+byte pin_rows_1[ROW_NUM] = {23, 25, 27, 29}; //connect to the row pinouts of the keypad
+byte pin_column_1[COLUMN_NUM] = {31, 33, 35, 37}; //connect to the column pinouts of the keypad
+byte pin_rows_2[ROW_NUM] = {39, 41, 43, 45}; //connect to the row pinouts of the keypad
+byte pin_column_2[COLUMN_NUM] = {47, 49, 51, 53}; //connect to the column pinouts of the keypad
+Keypad keypad_1 = Keypad( makeKeymap(keys_1), pin_rows_1, pin_column_1, ROW_NUM, COLUMN_NUM );
+Keypad keypad_2 = Keypad( makeKeymap(keys_2), pin_rows_2, pin_column_2, ROW_NUM, COLUMN_NUM );
 void setup(){
   Serial.begin(9600);
 }
 
 void loop(){
-  char key1 = keypad.getKey();
-  char key2= keypad1.getKey();
-  if (key1){
-    Serial.println(key);
-  }
-  if(key2)
-  {
-    Serial.println(key1);
-  }
-  }
+  char key_1 = keypad_1.getKey();
+  char key_2 = keypad_2.getKey();
+  
+  if (key_1)
+    Serial.write(key_1);
+  if(key_2)
+    Serial.write(key_2);
+}
